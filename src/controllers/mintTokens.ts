23fc661f53dff319c0ba0fa5f4c1ac3e61202681
Rishi Kant <kantrishi7779@gmail.com>
2025-06-20 02:23:16 +0530

    basic version for exchanging native tokens to mint rSOL

--- src/controllers/mintTokens.ts
+++ src/controllers/mintTokens.ts
@@ -1,15 +1,40 @@
-import { getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+import { getOrCreateAssociatedTokenAccount, mintTo, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import bs58 from 'bs58';
+import dotenv from 'dotenv'
 
+dotenv.config();
 
-const walletUint8Array = new Uint8Array(process.env.PRIVATE_KEY as any);
-const wallet = Keypair.fromSecretKey(walletUint8Array);
-export default async function mintTokens(fromAddress: PublicKey) {
+const secretKeyString = process.env.PRIVATE_KEY;
+if (!secretKeyString) throw new Error("PRIVATE_KEY not set in .env");
+
+const secretKey = bs58.decode(secretKeyString);
+const wallet = Keypair.fromSecretKey(secretKey);
+
+const mint = new PublicKey(process.env.MINT_PUBLIC_KEY as string);
+
+export default async function mintTokens(fromAddress: PublicKey, amount: number) {
+
     const connection = new Connection('https://api.devnet.solana.com');
-
-    const minttokens = getOrCreateAssociatedTokenAccount(
+    const ata = await getOrCreateAssociatedTokenAccount(
         connection,
         wallet,
-        
+        mint,
+        fromAddress,
+        false,
+        undefined,
+        undefined,
+        TOKEN_PROGRAM_ID
     )
+
+    const mintTokensTxHash = await mintTo(
+        connection,
+        wallet,
+        mint,
+        ata.address,
+        wallet.publicKey,
+        amount
+    )
+
+    return mintTokensTxHash
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import http from 'http'
-import { createToken } from './controllers/createLST';
-
+import { PublicKey } from '@solana/web3.js';
+import mintTokens from './controllers/mintTokens';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
+const data = {
+    "nativeTransfers": [{
+        "amount":
+            10000000,
+        "fromUserAccount": "9ozcPjeq8Vm5z3VVnFrmPpJMGuU2LqnPubYdS5qsrA47",
+        "toUserAccount": "2M7ntBVJMWsKhioRWxbYLYJemvJ2TrZ7BuDLVpTpEira"
+    }],
+}
 
-app.post("/helius", createToken)
+
+app.get("/helius", async (req: Request, res: Response) => {
+
+    const incomingData = data.nativeTransfers;
+    const myPublicKey = process.env.PUBLIC_KEY;
+    const fromAddress = incomingData.find(tx => tx.toUserAccount === myPublicKey)
+    const amount = fromAddress?.amount;
+
+    if (!fromAddress) {
+        res.send("no senders")
+        return;
+    }
+    const fromAdressKey = new PublicKey(fromAddress.fromUserAccount);
+    const mintTokensTxHash = await mintTokens(fromAdressKey, Number(amount));
+    res.send(mintTokensTxHash)
+    return;
+})
 
 
 server.listen(8080, () => {
